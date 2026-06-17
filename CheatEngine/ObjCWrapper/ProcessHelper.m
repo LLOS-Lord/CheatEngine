1a118d4e5474c80e6b34c61fcb1c6ccf678dd0e3
LLOS Lord <chungphan1607@gmail.com>
2026-06-17 13:14:27 +0700

    Update ProcessHelper.m

--- CheatEngine/ObjCWrapper/ProcessHelper.m
+++ CheatEngine/ObjCWrapper/ProcessHelper.m
@@ -1,7 +1,6 @@
 #import "ProcessHelper.h"
 #include <sys/sysctl.h>
 #include <sys/types.h>
-#include <libproc.h>
 
 @implementation RunningProcessInfo
 @end
@@ -28,13 +27,9 @@
         RunningProcessInfo *info = [[RunningProcessInfo alloc] init];
         info.pid = proc->kp_proc.p_pid;
         
-        char name[PROC_PIDPATHINFO_MAXSIZE] = {0};
-        if (proc_pidpath(info.pid, name, sizeof(name)) > 0) {
-            NSString *fullPath = [NSString stringWithUTF8String:name];
-            info.name = [fullPath lastPathComponent];
-        } else {
-            info.name = [NSString stringWithUTF8String:proc->kp_proc.p_comm];
-        }
+        // Sử dụng tên ngắn từ p_comm (không cần proc_pidpath)
+        info.name = [NSString stringWithUTF8String:proc->kp_proc.p_comm];
+        
         [list addObject:info];
     }
     free(procs);
